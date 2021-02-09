96a9af8bae7dbc123e983c29f9876cbbaafd4fe9
Hasanaga <hasanaga.mammadov@gmail.com>
2021-02-09 15:23:33 +0400

    modify main.php file

--- php/main.php
+++ php/main.php
@@ -1,1 +1,19 @@
-<?php echo 'hello'; ?>
+<?php 
+
+
+$bool = false;
+$sum = 100;
+
+if($bool == true){
+    echo "true\n";
+}else {
+    echo "false\n";
+}
+
+
+if(1==1){
+    echo "equals\n";
+}
+
+
+?>
